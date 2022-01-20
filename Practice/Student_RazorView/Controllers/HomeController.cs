1c293e8e4aae729427ad7ee45fc999f2e5fd3e7c
ManasiRanaware-BenchmarkitSolutions <97731232+ManasiRanaware-BenchmarkitSolutions@users.noreply.github.com>
2022-01-20 21:25:40 +0530

    Add files via upload

---
+++ Practice/Student_RazorView/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace viewdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    
+
+
+    public ActionResult StudentDetails()
+    {
+        ViewBag.StudentId = 101;
+        ViewBag.StudentName = "Scott";
+        ViewBag.Marks = 89;
+        ViewBag.Noofsemesters = 6;
+            ViewBag.Subjects = new List<string>() { "ENG", "Maths", "Science", "ART" };
+
+        return View();
+    }
+}
+}
