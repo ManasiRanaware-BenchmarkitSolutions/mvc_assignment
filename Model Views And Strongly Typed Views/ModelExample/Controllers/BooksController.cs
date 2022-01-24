7938afe2b366b0d102ad18170b9f9a147750678d
ranawaremanasi <97731232+ManasiRanaware-BenchmarkitSolutions@users.noreply.github.com>
2022-01-24 22:33:38 +0530

    Model

---
+++ Model Views And Strongly Typed Views/ModelExample/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelExample.Models;
+
+
+namespace ModelExample.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            List<Books> books = new List<Books>()
+            {
+                new Books() { BookId = 101, AuthorName = "A.P.J", Rate = 450 },
+                new Books() { BookId = 102, AuthorName = "Anurima Roy", Rate = 380 },
+                new Books() { BookId = 103, AuthorName = "Ruskin Bond", Rate = 940 }
+            };
+            return View(books);
+        }
+
+        public ActionResult Details(int id)
+        {
+            List<Books> books = new List<Books>()
+            {
+                new Books() { BookId = 101, AuthorName = "A.P.J", Rate = 450 },
+                new Books() { BookId = 102, AuthorName = "Anurima Roy", Rate = 380 },
+                new Books() { BookId = 103, AuthorName = "Ruskin Bond", Rate = 940 }
+            };
+            Books matchingBook = null;
+            foreach (var item in books)
+            {
+                if (item.BookId == id)
+                {
+                    matchingBook = item;
+                }
+            }
+            return View(matchingBook);
+        }
+    }
+}
---
+++ Model Views And Strongly Typed Views/ModelExample/Models/Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelExample.Models
+{
+    public class Books
+    {
+        public int BookId { get; set; }
+        public string AuthorName { get; set; }
+        public double Rate { get; set; }
+    }
+}
